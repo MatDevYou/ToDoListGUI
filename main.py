8de8b9c72156a2883064d971f799e2da127dca00
MatDev <cod4882@gmail.com>
2024-07-13 01:07:40 +0200

    aggiunta listbox

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from tkinter import  *
+from tkinter import *
+from tkinter import Listbox
 
 main = Tk()
 
+# titolo finestra
+main.title("ToDoList") 
+
+# dimensioni finestra
 main.geometry("900x600")
 
+# Creazione della Listbox prima del ciclo principale
+listbox = Listbox(main, width=50, height=25)
+listbox.pack(side=LEFT, fill=BOTH)
+
+# Avvio del ciclo principale
 main.mainloop()
